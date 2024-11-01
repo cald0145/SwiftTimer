4357efe2ebc1d5be908360f0c342f6d044b3c025
Jay Calderon <cald0145@algonquinlive.com>
2024-11-01 11:48:20 -0400

    Initial Commit

---
+++ SwiftTimer/SwiftTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTimerApp.swift
+//  SwiftTimer
+//
+//  Created by Jay Calderon on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTimerTests/SwiftTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTimerTests.swift
+//  SwiftTimerTests
+//
+//  Created by Jay Calderon on 2024-11-01.
+//
+
+import Testing
+@testable import SwiftTimer
+
+struct SwiftTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
